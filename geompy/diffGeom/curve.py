a8aa5adfe1887a9724e2e6d4ec241b549f42340a
AlexKaracaoglu <karacaog@bc.edu>
2018-03-08 08:35:35 -0500

    Unit speed verification
    Allow computations of Frenet Frame only when curve is a unit speed curve

--- geompy/diffGeom/curve.py
+++ geompy/diffGeom/curve.py
@@ -24,6 +24,10 @@ class Curve(GeometrySet):
     def parameter(self):
         return self.args[1]
 
+    @property
+    def dimension(self):
+        return len(self.args[0])
+
     def dot(v1, v2):
         result = np.dot(v1.functions,v2.functions)
         if type(result) is np.int32:
@@ -60,20 +64,38 @@ class Curve(GeometrySet):
         d = self.diffV()
         return d.length() == 1
 
+    def isUnitLength(self):
+        d = self.diffV()
+        return d.length() == 1
+
     def uTangent(self):
+        if self.dimension != 3:
+            raise ValueError("Finding the Tangent curve requires 3 dimensions "
+                "but got %s dimensions" % str(self.dimension))
+        if not self.isUnitLength():
+            raise ValueError("Using uTangent requires a unit speed curve "
+                "but input curve is not")
         return self.diffV()
 
     def uNormal(self):
+        if self.dimension != 3:
+            raise ValueError("Finding the Normal curve requires 3 dimensions "
+                "but got %s dimensions" % str(self.dimension))
+        if not self.isUnitLength():
+            raise ValueError("Using uNormal requires a unit speed curve "
+                "but input curve is not")
         tangentPrime = (self.uTangent()).diffV()
         tPrimeLength = tangentPrime.length()
         if (tPrimeLength != 0):
             tangentPrimeFunctions = list(((np.array(tangentPrime.functions))/ tPrimeLength))
-
             return Curve(tangentPrimeFunctions, self.parameter)
         else:
             return "undefined"
 
     def uCurvature(self):
+        if self.dimension != 3:
+            raise ValueError("Finding the curvature of the curve requires "
+                "3 dimensions but got %s dimensions" % str(self.dimension))
         tangentPrime = self.uTangent().diffV()
         return tangentPrime.length()
 
@@ -84,6 +106,12 @@ class Curve(GeometrySet):
         return results
 
     def uBinormal(self):
+        if self.dimension != 3:
+            raise ValueError("Finding the Binormal curve requires 3 dimensions "
+                "but got %s dimensions" % str(self.dimension))
+        if not self.isUnitLength():
+            raise ValueError("Using uBinormal requires a unit speed curve "
+                "but input curve is not")
         normal = self.uNormal()
         tangent = self.uTangent()
         if type(normal) is str:
@@ -92,9 +120,12 @@ class Curve(GeometrySet):
             return tangent.cross(normal)
 
     def uTorsion(self):
+        if self.dimension != 3:
+            raise ValueError("Finding the curvature of the curve requires "
+                "3 dimensions but got %s dimensions" % str(self.dimension))
         binormalPrime = self.diffV()
         normal = self.uNormal()
-        return dot(binormalPrime,normal)
+        return binormalPrime.dot(normal)
 
     def diffV(self):
             result = []
