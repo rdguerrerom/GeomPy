dd10386e088f0fb0297b962a45a3872d44dc428d
Alex Karacaoglu <31391932+AlexKaracaoglu@users.noreply.github.com>
2018-03-17 16:36:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'geompy',
-  packages = ['geompy'], # this must be the same as the name above
+  packages = ['geompy'],
   version = '0.1',
   description = 'A super cool differential geometry library',
   author = 'Alex Karacaoglu',
   author_email = 'karacaog@bc.edu',
   url = 'https://github.com/AlexKaracaoglu/geompy', 
-  download_url = 'https://github.com/AlexKaracaoglu/geompy/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['mathematics', 'geometry'], # arbitrary keywords
+  download_url = 'https://github.com/AlexKaracaoglu/geompy/archive/0.1.tar.gz',
+  keywords = ['mathematics', 'geometry'],
   classifiers = [],
+  install_requires=['numpy','sympy'],
 )
