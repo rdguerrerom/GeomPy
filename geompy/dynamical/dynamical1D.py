3b7f610f9eb227f830b3a1b6dad3b4eb89e1bcf8
AlexKaracaoglu <karacaog@bc.edu>
2018-05-22 14:56:53 -0400

    #26 DynamicalSytem1D Plot upgrade
    When running drawPortrait, the graph is now show to provide more info about the system and stability

--- geompy/dynamical/dynamical1D.py
+++ geompy/dynamical/dynamical1D.py
@@ -7,6 +7,7 @@ DynamicalSystem1D
 
 """
 
+from scipy import linspace
 from sympy import integrate
 from sympy.abc import *
 from sympy.core.basic import Basic
@@ -14,6 +15,7 @@ from sympy.core import sympify
 from sympy.solvers import solveset
 from sympy import S
 import matplotlib.pyplot as plt
+import numpy as np
 
 class DynamicalSystem1D(Basic):
     """A 1 Dimensional Dynamical System
@@ -22,20 +24,20 @@ class DynamicalSystem1D(Basic):
 
     Parameters
     ==========
-    
+
     system : a function
     parameter: symbol, ie. 'x'
 
     Attributes
     ==========
-    
+
     system
     parameter
     fuxedPoints
 
     Examples
     ========
-    
+
     >>> from dynamical1D import DynamicalSystem1D
     >>> xDot = DynamicalSystem1D((x**2) - 1, x)
     >>> xDot
@@ -43,20 +45,21 @@ class DynamicalSystem1D(Basic):
     >>> yDot = DynamicalSystem1D(y**2, y)
     >>> yDot
     DynamicalSystem1D(y**2, y)
-    
+
     """
-    
+
     def __repr__(self):
         return type(self).__name__ + repr(self.args)
-    
+
     def __str__(self):
         from sympy.printing import sstr
         ret = sstr(self.args)
         return type(self).__name__ + sstr(self.args)
-    
-    def __new__(cls, *args):
+
+    def __new__(cls, system, parameter):
+        args = [system, parameter]
         args = [sympify(a) for a in args]
-        return Basic.__new__(cls, *args)
+        return Basic.__new__(cls, system, parameter)
 
     @property
     def system(self):
@@ -64,12 +67,12 @@ class DynamicalSystem1D(Basic):
 
         Returns
         =======
-        
+
         system : a function that represents the system
-        
+
         Examples
         ========
-        
+
         >>> from dynamical1D import DynamicalSystem1D
         >>> xDot = DynamicalSystem1D((x**2) - 1, x)
         >>> xDot.system
@@ -87,12 +90,12 @@ class DynamicalSystem1D(Basic):
 
         Returns
         =======
-        
+
         parameter : symbol that represents the parameter of the system
-        
+
         Examples
         ========
-        
+
         >>> from dynamical1D import DynamicalSystem1D
         >>> xDot = DynamicalSystem1D((x**2) - 1, x)
         >>> xDot.parameter
@@ -107,10 +110,10 @@ class DynamicalSystem1D(Basic):
     @property
     def fixedPoints(self):
         """The fixed points of the 1D system
-        
+
         Examples
         ========
-        
+
         >>> from dynamical1D import DynamicalSystem1D
         >>> xDot = DynamicalSystem1D((x**2) - 1, x)
         >>> xDot.fixedPoints
@@ -126,10 +129,10 @@ class DynamicalSystem1D(Basic):
 
     def getEvalPoints(self):
         """The points to look at when evaluating stability of the 1D system
-        
+
         Examples
         ========
-        
+
         >>> from dynamical1D import DynamicalSystem1D
         >>> xDot = DynamicalSystem1D((x**2) - 1, x)
         >>> xDot.getEvalPoints()
@@ -160,10 +163,10 @@ class DynamicalSystem1D(Basic):
 
     def getEvalPointValues(self):
         """The point values to look at when evaluating stability of the 1D system
-        
+
         Examples
         ========
-        
+
         >>> from dynamical1D import DynamicalSystem1D
         >>> xDot = DynamicalSystem1D((x**2) - 1, x)
         >>> xDot.getEvalPointValues()
@@ -178,13 +181,13 @@ class DynamicalSystem1D(Basic):
         for evalPoint in evalPoints:
             result.append(self.system.subs(self.parameter, evalPoint))
         return result
-                          
+
     def classify(self):
         """The stability classification of the 1D system
-        
+
         Examples
         ========
-        
+
         >>> from dynamical1D import DynamicalSystem1D
         >>> xDot = DynamicalSystem1D((x**2) - 1, x)
         >>> xDot.classify()
@@ -210,7 +213,7 @@ class DynamicalSystem1D(Basic):
 
         Examples
         ========
-        
+
         >>> from dynamical1D import DynamicalSystem1D
         >>> xDot = DynamicalSystem1D((x**2) - 1, x)
         >>> xDot.potential()
@@ -218,15 +221,39 @@ class DynamicalSystem1D(Basic):
         >>> yDot = DynamicalSystem1D(y**2, y)
         >>> yDot.potential()
         -y**3/3
-        
+
         """
-        
+
         V = -integrate(self.system, self.parameter)
         return V
 
+    def solveSystem(self, value):
+        """The result when substituting the value into the system's parameter
+
+        Examples
+        ========
+
+        >>> from dynamical1D import DynamicalSystem1D
+        >>> xDot = DynamicalSystem1D((x**2) - 1, x)
+        >>> xDot.solveSystem(5)
+        24
+
+        """
+        return self.system.subs(self.parameter,value)
+
+    def solveSystemPlot(self, points):
+        """Creates the array that will correspond to the plot in drawPortrait
+        """
+        result = np.array([])
+        for point in points:
+            result = np.insert(result, len(result), self.solveSystem(point))
+        return result
+
+
+
     def drawPortrait(self):
         """The stability portrait for the 1D system
-        
+
         To run, simply call self.drawPortrait() and observe the plot.
         The stability portrait will be drawn and will label the
         stable, unstable and semi-stable fixed points for the dynamical
@@ -239,14 +266,15 @@ class DynamicalSystem1D(Basic):
         evalPoints = self.getEvalPoints()
         values = self.getEvalPointValues()
         plt.ylim(-.5,.5)
-        plt.xlim(float(fixedPoints[0]-1.5), float(fixedPoints[len(fixedPoints)-1]+1.5))
+        t = linspace(float(fixedPoints[0]-1.5), float(fixedPoints[len(fixedPoints)-1]+1.5), 100)
+        plt.plot(t, self.solveSystemPlot(t), zorder=1)
         for fp in results:
             if fp[1] == "Unstable":
-                plt.scatter(fp[0],0, color="none", edgecolor="black", label="Unstable Fixed Point")
+                plt.scatter(fp[0],0, color="white", edgecolor="black", label="Unstable Fixed Point", zorder=2)
             elif fp[1] == "Stable":
-                plt.scatter(fp[0],0, color="black", edgecolor="black", label="Stable Fixed Point")
+                plt.scatter(fp[0],0, color="black", edgecolor="black", label="Stable Fixed Point", zorder=2)
             else:
-                plt.scatter(fp[0],0, color="yellow", edgecolor="yellow", label="Semi-Stable")
+                plt.scatter(fp[0],0, color="red", edgecolor="red", label="Semi-Stable", zorder=2)
         for i in range(len(evalPoints)):
             if values[i] > 0:
                 plt.arrow(evalPoints[i], 0, 0.001, 0, head_width=0.1, head_length=.30, fc="blue")
